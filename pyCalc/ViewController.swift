df7ec628a3042b83bfd589cefbd9e9368bc08f37
davidanew <davidanew@googlemail.com>
2019-04-30 16:47:46 +0100

    Basic calculation working

--- pyCalc/ViewController.swift
+++ pyCalc/ViewController.swift
@@ -27,13 +27,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var correctedTime: UILabel!
     
+    //let elapsedSeconds = BehaviorSubject<Int32>(value: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         Observable.just([Array(0...9).map{"\($0)"}])
             .bind(to: timeHours.rx.items(adapter: PickerViewViewAdapter()))
             .disposed(by: disposeBag)
+        
+        //TODO: sort out significant fingures
         
         Observable.just([Array(0...59).map{"\($0)"}])
             .bind(to: timeMinutes.rx.items(adapter: PickerViewViewAdapter()))
@@ -43,8 +46,59 @@ class ViewController: UIViewController {
             .bind(to: timeSeconds.rx.items(adapter: PickerViewViewAdapter()))
             .disposed(by: disposeBag)
         
+        let elapsedHoursInSeconds = timeHours.rx.modelSelected(String.self).map{ 3600 * (Int32($0.first ?? "0") ?? 0) }.startWith(0)
+        let elapsedMinutesInSeconds = timeMinutes.rx.modelSelected(String.self).map{ 60 * (Int32($0.first ?? "0") ?? 0) }.startWith(0)
+        let elapsedSecondsInSeconds = timeSeconds.rx.modelSelected(String.self).map{ 1 * (Int32($0.first ?? "0") ?? 0) }.startWith(0)
+        let elapsedInSeconds = Observable.combineLatest( elapsedHoursInSeconds, elapsedMinutesInSeconds, elapsedSecondsInSeconds, resultSelector : {value1, value2, value3 in
+            return value1 + value2 + value3
+        })
+        
+        let completedLaps = laps.rx.modelSelected(String.self).map{ Int32($0.first ?? "") ?? 1}.startWith(1)
+        let maximumLaps = maxLaps.rx.modelSelected(String.self).map{ Int32($0.first ?? "") ?? 1}.startWith(1)
+        let lapCorrection = Observable.combineLatest(completedLaps, maximumLaps, resultSelector : {value1, value2 in
+            return value2 / value1
+        })
+        
+        let pyThousandsInUnits = pyThousands.rx.modelSelected(String.self).map{ 1000 * (Int32($0.first ?? "") ?? 0) }.startWith(0)
+        let pyHundredsInUnits = pyHundreds.rx.modelSelected(String.self).map{ 100 * (Int32($0.first ?? "") ?? 0) }.startWith(0)
+        let pyTensInUnits = pyTens.rx.modelSelected(String.self).map{ 10 * (Int32($0.first ?? "") ?? 0) }.startWith(0)
+        let pyUnitsInUnits = pyUnits.rx.modelSelected(String.self).map{ 1 * (Int32($0.first ?? "") ?? 0) }.startWith(0)
+        let py = Observable.combineLatest(pyThousandsInUnits, pyHundredsInUnits, pyTensInUnits, pyUnitsInUnits, resultSelector: {value1, value2, value3, value4 in
+            return value1 + value2 + value3 + value4
+        } )
+        
+        let correctedTime = Observable.combineLatest(elapsedInSeconds, lapCorrection, py, resultSelector : {(elapsedInSeconds, lapCorrection, py) -> Int32 in
+            if py > 0 {
+                let correctedTimeValue = elapsedInSeconds * lapCorrection * 1000 / py
+                print ("Corrected time is \(correctedTimeValue)")
+                return correctedTimeValue
+            } else {
+                return 0
+            }
+            
+        })
+        
+        correctedTime.map{Optional(String($0))}.bind(to: self.correctedTime.rx.text).disposed(by: disposeBag)
+
+        //TODO: output should be blank for error
+        //TODO: changed corrected time text to info
+        //TODO: put this in functions
+        
 
         
+        
+        _ = elapsedInSeconds.subscribe(onNext: {print  ($0)})
+        
+        _ = lapCorrection.subscribe(onNext: {print ($0)})
+        
+        _ = py.subscribe(onNext: {print ($0)})
+        
+        _ = correctedTime.subscribe(onNext: {print ($0)})
+
+
+
+
+
         
         
         Observable.just([Array(1...99).map{"\($0)"}])
@@ -89,7 +143,7 @@ final class PickerViewViewAdapter
     , UIPickerViewDataSource
     , UIPickerViewDelegate
     , RxPickerViewDataSourceType
-, SectionedViewDataSourceType {
+    , SectionedViewDataSourceType {
     typealias Element = [[CustomStringConvertible]]
     private var items: [[CustomStringConvertible]] = []
     
